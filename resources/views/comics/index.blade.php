920c4a883082ef264dca53dad6c7055a15d001d6
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-29 15:31:12 +0200

    Add → added Index Comic CRUD (View/Controller/Route)

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title', 'Comics - Index')
+
+@section('main-content')
+    <section class="mt-5 py-1">
+        <div class="container bg-dark py-4">
+            <h1 class="title text-center text-warning">Welcome to my Comics Index!</h1>
+        </div>
+    </section>
+
+
+    <section class="mb-5 py-1">
+        <div class="bg-light container py-4">
+            <p class="title text-center text-dark">Qui stamperemo i fumetti</p>
+        </div>
+    </section>    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ResourceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
+
+// Comic CRUD
+
+// Index
+Route::get('/comics',[ResourceController::class, 'index'])->name('comics.index');
+
+// Create
+
+
+
+// Show
+
+
+
+// Store

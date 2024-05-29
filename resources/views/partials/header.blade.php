f3979d6223a09b3511ed22cf5b901690e7f36ae7
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-29 15:30:07 +0200

    Add → added anchors to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,22 @@
 <header>
-    <section class="bg-light">        
+    <section class="bg-light">
         <div class="container">
-            <nav class="py-3">
-                <div class="row">
-                    <div class="col">
-                        <h1 class="text-success text-center">
-                            Header
-                        </h1>
-                    </div>                                                        
-                </div>
-            </nav>
+            <h1 class="text-success text-center">
+                Header
+            </h1>
         </div>
+        <nav class="py-3">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-2 text-center">
+                        <a href="{{route('home')}}">Home</a>
+                    </div>
+                    <div class="col-2 text-center">
+                        <a href="{{route('comics.index')}}">Comics</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
     </section>
 </header>
